e118a982510ea56455c65b1c38dbef9d163dee3c
Hanne Paine <paine.hanne@gmail.com>
2014-04-02 00:32:39 -0400

    Clean up the project a bunch.
    I don't think we don't need all the example code.
    I also think we should hold off on adding Google Web Toolkit stuff
    until we know we're going to use it. Let's set up the javascript
    and CSS stuff first.

---
+++ AlgebraThing/src/com/jazzberryjam/algebra_thing/AlgebraThing.java
@@ -0,0 +1,11 @@
+package com.jazzberryjam.algebra_thing;
+
+import javax.servlet.http.HttpServlet;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class AlgebraThing extends HttpServlet {
+
+}
