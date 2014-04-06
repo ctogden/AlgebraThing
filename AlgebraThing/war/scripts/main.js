54a619beaeeb5de3a1d882aacf144b05b520b8b3
Varsha_Paidi <varsha_paidi@hotmail.com>
2014-04-05 23:03:51 -0400

    Added basic function buttons.

--- AlgebraThing/war/scripts/main.js
+++ AlgebraThing/war/scripts/main.js
@@ -1,5 +1,28 @@
-/*Main script file for Angularjs*/
+//Main script file for the application
+$(function(){
+		
+        // On loading the initial page, it also loads the modal
+		$(window).load(function(){
+		        $('#welcome').modal('show');
+		        $('#function_bar').hide();
+		    });
+		 
+		// Popover for Equation Help
+		$('#equation_help').popover({
+	 		trigger: 'click',
+		    placement: 'right',
+		    title: 'Help'
+	 	});
+		
+		// Popover for more operations
+		$('#more_functions').popover({
+	 		trigger: 'click',
+		    placement: 'right'
+	 	});
+		
+});
 
+//Angular Controller
 function MainController($scope){
 	
 	$scope.equations = [];
@@ -8,6 +31,9 @@ function MainController($scope){
 	{
 		$scope.equations.push($scope.newEquation);
         $scope.newEquation = "";
+        $('#equation_editor').hide();
+        $('#equation_help').hide();
+        $('#function_bar').show();
 	}
 	
 }
