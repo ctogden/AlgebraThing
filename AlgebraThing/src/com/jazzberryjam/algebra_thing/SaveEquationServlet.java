d4ab047ccec504b223c2ebddb8be7ce3c89e8a4e
Scott MacDonell <scott.macdonell@stonybrook.edu>
2014-04-16 16:08:55 -0700

    Equation saving in database on server working.

---
+++ AlgebraThing/src/com/jazzberryjam/algebra_thing/SaveEquationServlet.java
@@ -0,0 +1,94 @@
+package com.jazzberryjam.algebra_thing;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.mortbay.util.ajax.JSON;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+
+public class SaveEquationServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	JSONObject equationJson = null;
+    	try {
+			equationJson = new JSONObject(request.getParameter("equationData"));
+		} catch (JSONException e) {
+			e.printStackTrace(System.err);
+		}
+    	
+    	Iterator it = equationJson.keys();
+    	String equation = null;
+    	while(it.hasNext()) {
+    		equation = (String) it.next();
+    	}
+    	
+    	DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Key equationKey = KeyFactory.createKey("equation", "equation");
+        Entity equationEntity = new Entity("equation", equationKey);
+        equationEntity.setProperty("equation", equation);
+        datastore.put(equationEntity);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ AlgebraThing/src/com/jazzberryjam/algebra_thing/validation/PasswordEqualityValidator.java
@@ -0,0 +1,28 @@
+package com.jazzberryjam.algebra_thing.validation;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("passwordEqualityValidator")
+public class PasswordEqualityValidator implements Validator {
+
+	@Override
+	public void validate(FacesContext context, UIComponent component, Object value)
+			throws ValidatorException {
+		String pword = (String) value;
+		String confPword = (String) component.getAttributes().get("regConfPword");
+		
+		if(pword == null || confPword == null) {
+			return;
+		}
+		
+		if(!pword.equals(confPword)) {
+			throw new ValidatorException(new FacesMessage("Your passwords must match"));
+		}
+	}
+
+}
